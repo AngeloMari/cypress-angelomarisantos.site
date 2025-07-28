3201aad2dd391434def812602f8869903a7db1a9
AngeloMari <santosangelomari@gmail.com>
2025-07-28 17:21:31 +0800

    enhanced layout and styling

---
+++ pages/scrollToTop.js
@@ -0,0 +1,13 @@
+const backToTop = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    backToTop.classList.add("show");
+  } else {
+    backToTop.classList.remove("show");
+  }
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
