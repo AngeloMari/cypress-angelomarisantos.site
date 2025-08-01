404bf571182cd6c5391df0dde18a03d153257982
AngeloMari <santosangelomari@gmail.com>
2025-08-01 16:39:48 +0800

    Fixed a bug in the toast notification.

--- scripts/toastNotification.js
+++ scripts/toastNotification.js
@@ -13,9 +13,4 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault(); // prevents the anchor from navigating
     showToast("This section is still under development. :))");
-
-    // if you still want to redirect after the toast:
-    setTimeout(() => {
-      window.location.href = this.href;
-    }, 1000); // delay navigation so toast is visible
   });
